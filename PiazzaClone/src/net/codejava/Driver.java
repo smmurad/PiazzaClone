9ba190e403eac4e467faa634563db9479e6b342c
smmurad <muradsigurd@gmail.com>
2021-03-16 17:19:11 +0100

    Added to gitignore

--- PiazzaClone/src/net/codejava/Driver.java
+++ PiazzaClone/src/net/codejava/Driver.java
@@ -5,9 +5,12 @@ import java.sql.*;
 public class Driver {
 
 	public static void main(String[] args) {
-		System.out.println("Test");
-		// Added comment
-		// Added another comment
-		// Added one more comment
+		try {
+			Connection conn = DriverManager.getConnection("");
+		}
+		catch(Exception e)
+		{
+			
+		}
 	}
 }
---
+++ PiazzaClone/src/net/codejava/UserManager.java
@@ -0,0 +1,10 @@
+package net.codejava;
+
+public class UserManager {
+
+	public static void main(String[] args) {
+		string results = Driver.GetData()
+
+	}
+
+}
