31c9eb3a179f7c873ccfbf21a91a56639994ae25
smmurad <muradsigurd@gmail.com>
2021-03-16 18:11:55 +0100

    Added driver functionality and UserManager data

--- PiazzaClone/src/net/codejava/Driver.java
+++ PiazzaClone/src/net/codejava/Driver.java
@@ -1,12 +1,25 @@
 package net.codejava;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Driver {
 
 	public static void main(String[] args) {
-		try {
-			Connection conn = DriverManager.getConnection("");
+		try 
+		{
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/piazza", "root", "12345678");
+			
+			Statement statement = conn.createStatement();
+			
+			ResultSet result = statement.executeQuery("SELECT * FROM colorcode;");
+			System.out.println("test");
+			
+			while(result.next()) {
+				System.out.println(result.getString(1) + ", " + result.getString(2));
+			}
+			
 		}
 		catch(Exception e)
 		{
--- PiazzaClone/src/net/codejava/UserManager.java
+++ PiazzaClone/src/net/codejava/UserManager.java
@@ -3,7 +3,6 @@ package net.codejava;
 public class UserManager {
 
 	public static void main(String[] args) {
-		string results = Driver.GetData()
 
 	}
 
